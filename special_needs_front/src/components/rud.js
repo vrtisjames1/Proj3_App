bbba9063d8122ea772692a3a74117250192e5a1f
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2022-12-09 11:25:16 -0600

    some update functions have been added and a map function was created. Not complete

--- special_needs_front/src/components/rud.js
+++ special_needs_front/src/components/rud.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
 
-
+// RUD - READ, UPDATE, DELETE 
 const RUD = () => {
     const [updateNewParent, setUpdateNewParent] = useState('')
     const [updateNewKid, setUpdateKid] = useState('')
@@ -23,3 +23,58 @@ const RUD = () => {
         })
     }
 }
+
+const updateParentInfo = (e) => {
+    setUpdateNewParent(e.target.value)
+}
+
+const updateKidName = (e) => {
+    setUpdateKid(e.target.value)
+}
+
+const updateKidPhoto = (e) => {
+    setUpdateNewPhoto(e.target.value)
+}
+
+// const updateStatus = (e) => {
+//     setUpdateNewStatus(e.target.value)
+// }
+
+return (
+    <>
+    {/* READ  */}
+    {students.map((student) => {
+        return (
+            <li>
+                <div className='card'>
+                    <div className='student-info'>
+                        {/* CHILD NAME */}
+                        <h3>{student.kid}</h3>
+                        <img src={student.image} /><br />
+                    </div>
+
+                    <div className='student-status'>
+                        <h4>{student.status.header}</h4>
+                        <h4>{student.status.date}</h4>
+                        <p>{student.status.comment}</p>
+                    </div>
+
+                    <div className='card-button'>
+                        <button on>Update</button>
+                    </div>
+                </div>
+            </li>
+        )
+    })}
+
+    {/* UPDATE FORM  */}
+        <form>
+            <input onChange={updateParentInfo} defaultValue={props.} /><br />
+            <input onChange={updateKidName} defaultValue={} /><br />
+            <input onChange={updateKidPhoto} defaultValue={} /><br />
+            {/* <input onChange={updateStatus} defaultValue={} /><br /> */}
+
+        </form>
+    </>
+)
+export default RUD
